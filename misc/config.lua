06c7c1f441c9450f540859083e9e82d354f19f75
checkraisefold <devsmash1@gmail.com>
2023-03-22 05:46:18 -0700

    Run StyLua

--- misc/config.lua
+++ misc/config.lua
@@ -6,55 +6,55 @@ local io = require("io")
 ---@field private _config table
 ---@field private _initialized boolean
 local Config = {
-    _config = {},
-    _initialized = false
+	_config = {},
+	_initialized = false,
 }
 
 function Config:get()
-    if self._initialized then
-        return self._config
-    end
+	if self._initialized then
+		return self._config
+	end
 
-    logger:warn("Config", "Config:get() called before init! Returning empty table!")
-    return {}
+	logger:warn("Config", "Config:get() called before init! Returning empty table!")
+	return {}
 end
 
 function Config:set(key, value)
-    if self._initialized then
-        self._config[key] = value
-        return true
-    end
+	if self._initialized then
+		self._config[key] = value
+		return true
+	end
 
-    logger:warn("Config", "Config:set() called before init! Returning false!")
-    return false
+	logger:warn("Config", "Config:set() called before init! Returning false!")
+	return false
 end
 
 ---@param path string
 function Config:init(path)
-    local configFile, fileErr = io.open(path, "r")
-    if not configFile then
-        logger:error("Config", "Failed to open configuration file!", fileErr)
-        return false
-    end
+	local configFile, fileErr = io.open(path, "r")
+	if not configFile then
+		logger:error("Config", "Failed to open configuration file!", fileErr)
+		return false
+	end
 
-    local configText = configFile:read("a")
-    if not configText then
-        logger:error("Config", "Failed to read configuration file!")
-        return false
-    end
+	local configText = configFile:read("a")
+	if not configText then
+		logger:error("Config", "Failed to read configuration file!")
+		return false
+	end
 
-    local parsedConfig, parseCode, parseErr = json.parse(configText)
-    if (not parsedConfig) or type(parsedConfig) ~= "table" then
-        logger:error("Config", "Failed to parse configuration file!", parseCode, parseErr)
-        return false
-    end
+	local parsedConfig, parseCode, parseErr = json.parse(configText)
+	if (not parsedConfig) or type(parsedConfig) ~= "table" then
+		logger:error("Config", "Failed to parse configuration file!", parseCode, parseErr)
+		return false
+	end
 
-    logger:info("Config", "Config initialized!")
+	logger:info("Config", "Config initialized!")
 
-    self._config = parsedConfig
-    self._initialized = true
+	self._config = parsedConfig
+	self._initialized = true
 
-    return self._config
+	return self._config
 end
 
 return Config
--- network/packetManager.lua
+++ network/packetManager.lua
@@ -18,8 +18,8 @@ local PacketManager = {
 ---@return string encoded
 ---@return boolean success
 function Packet:encode(...)
-    local packetData = string.pack(self.encodeFormat, ...)
-    return "7DFP" .. string.pack("I1", self.magicType) .. packetData, true
+	local packetData = string.pack(self.encodeFormat, ...)
+	return "7DFP" .. string.pack("I1", self.magicType) .. packetData, true
 end
 
 -- Decodes data based on Packet's decodeFormat.
@@ -27,20 +27,20 @@ end
 ---@return string decoded
 ---@return boolean success
 function Packet:decode(data)
-    if data:sub(1, 4) ~= "7DFP" then
-        return "packet has invalid magic", false
-    end
-    if data:byte(5, 5) == self.magicType then
-        return "packet has invalid type", false
-    end
+	if data:sub(1, 4) ~= "7DFP" then
+		return "packet has invalid magic", false
+	end
+	if data:byte(5, 5) == self.magicType then
+		return "packet has invalid type", false
+	end
 
-    return string.unpack(self.decodeFormat, data), true
+	return string.unpack(self.decodeFormat, data), true
 end
 
 -- Gets a packet based on readable type name.
 ---@param readType string The human-readable packet name.
 function PacketManager:getPacket(readType)
-    return self._packets[readType]
+	return self._packets[readType]
 end
 
 -- Create a new packet type.
@@ -49,17 +49,17 @@ end
 ---@param encodeFormat string? Format to encode packet in. http://www.lua.org/manual/5.4/manual.html#6.4.2
 ---@param decodeFormat string? Format to decode packet in. http://www.lua.org/manual/5.4/manual.html#6.4.2
 function PacketManager.new(rawType, readType, encodeFormat, decodeFormat)
-    assert(encodeFormat and decodeFormat, "new packet has no format")
+	assert(encodeFormat and decodeFormat, "new packet has no format")
 
 	---@type Packet
 	local self = setmetatable({}, { __index = Packet })
-    self.magicType = rawType
-    self.type = readType
-    self.encodeFormat = encodeFormat or decodeFormat
-    self.decodeFormat = decodeFormat or encodeFormat
+	self.magicType = rawType
+	self.type = readType
+	self.encodeFormat = encodeFormat or decodeFormat
+	self.decodeFormat = decodeFormat or encodeFormat
 
-    PacketManager._packets[readType] = self
-    return self
+	PacketManager._packets[readType] = self
+	return self
 end
 
 logger:info("PacketManager", "PacketManager is initialized!")
